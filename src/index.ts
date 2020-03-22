3ab705feda4965a5debd7069e4e3d8522338aa21
Thomas Howard Taft <whynotavailable@protonmail.com>
2020-03-22 14:08:58 -0400

    Added typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const express = require('express'),
-      bodyParser = require('body-parser'),
-      fs = require('fs'),
-      Mustache = require('mustache');
+import * as bodyParser from "body-parser";
+import * as fs from "fs";
+import * as express from 'express'
+import { render, parse } from 'mustache'
 
 const app = express();
 
@@ -11,14 +11,16 @@ const port = 3000;
 
 const loginForm = fs.readFileSync('templates/login-form.html').toString();
 
-Mustache.parse(loginForm);
+// Pre-compile the form
+parse(loginForm);
 
 // For healthchecks
 app.get('/ping', (req, res) => res.send('pong'));
 
-function renderLoginForm(res) {
-   res.header["Content-Type"] = "text/html";
-   res.send(Mustache.render(loginForm));
+function renderLoginForm(res: express.Response) {
+   // @ts-ignore
+   res.setHeader("Content-Type", "text/html");
+   res.send(render(loginForm, {}));
 }
 
 app.get('/', (req, res) => {
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+export function getTest(): string {
+   return 'Yolo';
+}
